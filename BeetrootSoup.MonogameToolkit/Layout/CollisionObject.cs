7dfce5bd6ed9f4643f31a276d29ad18ecd3f2ae9
JBarszczewski <jbarszczewski@outlook.com>
2015-03-29 18:37:23 +0100

    Added CollisionObject

---
+++ BeetrootSoup.MonogameToolkit/Layout/CollisionObject.cs
@@ -0,0 +1,13 @@
+﻿using BeetrootSoup.MonogameToolkit.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeetrootSoup.MonogameToolkit.Layout
+{
+    public class CollisionObject : Node
+    {
+        public RotatedRectangle ColisionRectangle { get; set; }
+    }
+}
