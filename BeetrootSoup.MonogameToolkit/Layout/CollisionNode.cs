d67db5cc34f88168262b220c07281cf4edc3e6e1
JBarszczewski <jbarszczewski@outlook.com>
2015-04-22 23:17:29 +0100

    Fixed Collision node update

--- BeetrootSoup.MonogameToolkit/Layout/CollisionNode.cs
+++ BeetrootSoup.MonogameToolkit/Layout/CollisionNode.cs
@@ -1,4 +1,5 @@
 ﻿using BeetrootSoup.MonogameToolkit.Helpers;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,18 @@ namespace BeetrootSoup.MonogameToolkit.Layout
     public class CollisionNode : Node
     {
         public RotatedRectangle ColisionRectangle { get; set; }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (this.ColisionRectangle != null)
+            {
+                var absolutePosition = this.GetAbsolutePosition();
+                this.ColisionRectangle.CollisionRectangle.X = (int)(absolutePosition.X - this.ColisionRectangle.Origin.X);
+                this.ColisionRectangle.CollisionRectangle.Y = (int)(absolutePosition.Y - this.ColisionRectangle.Origin.Y);
+                this.ColisionRectangle.Rotation = this.GetAbsoluteRotation();
+            }
+
+            base.Update(gameTime);
+        }
     }
 }
